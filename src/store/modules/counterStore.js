df66a3b32ab1682a7bcccd4d10472852a0d0b02e
der-bao <19270255344@163.com>
2026-02-02 22:36:02 +0800

    实现counter

---
+++ src/store/modules/counterStore.js
@@ -0,0 +1,29 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const counterStore = createSlice({
+    name: 'counter',
+    // 初始化状态
+    initialState:{
+        count: 0
+    },
+    // 修改状态的方法 同步方法 状态支持直接修改
+    reducers:{
+        increment(state){
+            state.count++
+        },
+        decrement(state){
+            state.count--
+        }
+    }
+})
+
+// 解构出来actionCreater函数
+const {increment, decrement} = counterStore.actions
+// 导出reducer函数
+const reducer = counterStore.reducer
+
+// 以按需导出的方式导出actionCreater函数
+export {increment, decrement}
+// 以默认导出的方式导出reducer函数
+export default reducer
+
